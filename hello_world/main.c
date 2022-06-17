c79349a21d439d60b29bc336e3d308eb4a3ad77b
Brayan <5181@holbertonschool.com>
2022-06-17 16:37:20 -0700

    Compiler4

--- hello_world/main.c
+++ hello_world/main.c
@@ -19,3 +19,26 @@ int main(void)
 	    return (0);
 
 }
+#include <stdio.h>
+
+
+
+/**
+ *
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Return: Always 0 (Success)
+ *
+ *     */
+
+int main(void)
+
+{
+
+		printf("Holberton School");
+
+			return (0);
+
+}
